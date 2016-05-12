2d39289019904e99ce58a7ca9ad112d73d86ef91
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-05-12 14:46:20 -0400

    Updated game to show winner

--- script.js
+++ script.js
@@ -1,7 +1,82 @@
 //debugger
-$(function () {
-  var turnCount = 0;
+/*var winningCombos = [
+  ["A1", "A2", "A3"],
+  ["B1", "B2", "B3"],
+  ["C1", "C2", "C3"],
+  ["A1", "B1", "C1"],
+  ["A2", "B2", "C2"],
+  ["A3", "B3", "C3"],
+  ["A1", "B2", "C3"],
+  ["A3", "B2", "C1"],
+];
+*/
 
+//array to show winning combos
+var winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
+
+//function that given:
+ //1. a player - "X" or "O"
+ //2. a winning combo - an array of squares
+ //3. the board - an array of all moves
+function playerOccupiesSquares(player, squares, board) {
+  //checks: if the player occupies all 3 squares of combo
+  //loop through squares
+  for (var i = 0; i < squares.length; i++) {
+    var boardIndex = squares[i];
+    var value = board[boardIndex];
+    //does one player occupy all three
+    if (value !== player) {
+      //if false: return false
+      return false;
+    }
+  }
+   //if true: return true
+   return true;
+}
+
+//function that returns an array consisting of the current moves on the board
+function getCurrentBoard() {
+  var buttons = $("button");
+  var moves = [];
+  for (var i = 0; i < buttons.length; i++) {
+    var button = $(buttons[i]);
+    moves.push(button.text());
+  }
+  return moves
+}
+
+//function that looks at the board and returns winner
+function checkWinner(board) {
+  //loop through combos
+  for (var i = 0; i < winningCombos.length; i++) {
+    //save current combo
+    var combo = winningCombos[i];
+    //for each player use playerOccupiesSquares function to check if they won
+    if (playerOccupiesSquares("O", combo, board)) {
+      //if it's true then return player "O" as winner
+      return "O";
+    }
+    if (playerOccupiesSquares("X", combo, board)) {
+      //if it's true then return player "X" as winner
+      return "X";
+    }
+  }
+  //if go through all combos and there is no winner, return null - winner is "O", "X", or nobody
+  return null;
+}
+
+var turnCount = 0;
+
+$(function () {
   //when you click a square (button)
   $("button").click(function () {
     var text = $(this).text();
@@ -9,14 +84,25 @@ $(function () {
     if (text === "O" || text === "X") {
       text = $(this).text();
     //first time change to "X"
-    } else if (turnCount % 2 === 0 && text === "") {
+    } else if (turnCount % 2 === 0) {
       text = $(this).text("X");
       turnCount++;
+      var board = getCurrentBoard();
+      var winner = checkWinner(board);
+      if (winner) {
+        alert("The winner is " + winner);
+      }
     //second time change to "O"
-    } else if (turnCount % 2 === 1 && text === "") {
+    } else if (turnCount % 2 === 1) {
       text = $(this).text("O");
       turnCount++;
+      var board = getCurrentBoard();
+      var winner = checkWinner(board);
+      if (winner) {
+        alert("The winner is " + winner);
+      }
     }
   });
+
 
 });
